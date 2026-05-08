4e3812db7ab00bebde3ad52156e4f9842fb43bd0
Tommy Evertsen <tommyevertsen52@gmail.com>
2026-05-08 23:22:24 +0200

    first commit

---
+++ Tasks.cs
@@ -0,0 +1,9 @@
+public class TaskItem
+{
+    public string? Title {get; set;}
+    public string? Description {get; set;}
+    public int RewardPoints {get; set;}
+    public bool Completed {get; set;}
+
+
+}
---
+++ Units.cs
@@ -0,0 +1,9 @@
+public class Units
+{
+    public int Hp {get; set;}
+    public string? Name {get; set;}
+    public int AttackPower {get; set;}
+    public int Defence {get; set;}
+
+
+}
